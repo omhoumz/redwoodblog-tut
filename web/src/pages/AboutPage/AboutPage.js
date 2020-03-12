41004ce69803fd5c9126422b78d4642b83c90639
Omar Houmz <omar.houmz@adevinta.com>
2020-03-12 18:20:35 +0100

    feat: add home & about pages and their routes

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import BlogLayout from 'src/layouts/BlogLayout/'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <h2>AboutPage</h2>
+      <p>Find me in ./web/src/pages/AboutPage/AboutPage.js</p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const HomePage = () => {
+  return (
+    <BlogLayout>
+      <h2>HomePage</h2>
+    </BlogLayout>
+  )
+}
+
+export default HomePage
